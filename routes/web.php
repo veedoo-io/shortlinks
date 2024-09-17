5888c888cd359c79355010dea98fe3423b683df0
Nikolay <mukola787898@gmail.com>
2024-09-17 22:28:50 +0300

    added redirect to start page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
-<?php
+<?php declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'web.index')->name('web.index');
+Route::redirect('/', env('REDIRECT_URL', 'https://euvsdisinfo.eu'), 301);
