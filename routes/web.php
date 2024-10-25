36b5ba14f380d21f5c01620faa0eb7b59ad3ce12
Nikolay <mukola787898@gmail.com>
2024-10-25 15:14:37 +0300

    change route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', env('REDIRECT_URL', 'https://euvsdisinfo.eu'), 301);
+//Route::redirect('/', env('REDIRECT_URL', 'https://euvsdisinfo.eu'), 301);
 
-Route::get('/gpt/create', [\App\Http\Controllers\GPT\GPTController::class, 'index'])->name('gpt.index');
-Route::post('/gpt/create', [\App\Http\Controllers\GPT\GPTController::class, 'create'])->name('gpt.create');
+Route::get('/', [\App\Http\Controllers\GPT\GPTController::class, 'index'])->name('gpt.index');
+Route::post('/', [\App\Http\Controllers\GPT\GPTController::class, 'create'])->name('gpt.create');
 
 Route::view('/gpt/auth', 'gpt.auth.index')->name('gpt.auth.index');
 Route::post('/gpt/auth', [\App\Http\Controllers\GPT\GPTController::class, 'auth'])->name('gpt.auth.save');
