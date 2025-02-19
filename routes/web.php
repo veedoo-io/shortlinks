57f782422fd4a913803297cbcfcde511d56f7d2a
Nikolay <mukola787898@gmail.com>
2025-02-19 15:28:13 +0200

    added status page

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', env('REDIRECT_URL', 'https://euvsdisinfo.eu'), 301);
 
-Route::view('/status/check', 'web.index')->name('check');
+Route::view('/status/check', 'status.index')->name('check');
